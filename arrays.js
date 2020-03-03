1c25920ca479ba782504c76afe28de2859402550
Danielle Smith <57499759+Danielle-Smith@users.noreply.github.com>
2020-03-03 10:35:11 -0700

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -70,3 +70,37 @@ arr; // ["Altuve", "Bregman", "Springer"]
 arr.splice(1, 2); // ["Bregman", "Springer"]   2 arguments: 1, index ; 2, number of items to be removed
 
 arr; // ["Altuve"]
+
+//   spread operator
+
+// Combining Arrays
+let shoppingCart = [345, 375, 765, 123];
+let newItems = [98, 123];
+
+shoppingCart.push(...newItems); // would normally add the array into the other array. the ... separates the items in the array.
+console.log(shoppingCart); // [345, 375, 765, 123, 98, 123]
+
+// Copying Arrays
+const shoppingCart = [345, 375, 765, 123];
+const updatedCart = [...shoppingCart]; // this prevents the updatedCart and shoppingCart from being linked and you can make changes to updatedCart without changing the original cart
+updatedCart.push(5);
+console.log(updatedCart);
+console.log(shoppingCart);
+
+const orderTotals = [1, 5, 1, 10, 2, 3];
+console.log(Math.max(...orderTotals)); // This separates the array into integers
+
+// [1, 5, 1, 10, 2, 3]
+// ...[1, 5, 1, 10, 2, 3]
+// 1, 5, 1, 10, 2, 3
+
+const { starter, closer, ...relievers } = { // You can use ... when you don't know how many relievers you have.
+  starter: 'Verlander',
+  closer: 'Giles',
+  relief_1: 'Morton',
+  relief_2: 'Gregerson'
+}
+
+console.log(starter);
+console.log(closer);
+console.log(relievers);
